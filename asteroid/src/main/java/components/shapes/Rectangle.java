d6aff2205d99e56dcf32b4c3edb13c0d3b1493a4
lalo <leandro.gz73@gmail.com>
2014-04-30 00:29:31 -0300

    Add collitions with rectangles

--- asteroid/src/main/java/components/shapes/Rectangle.java
+++ asteroid/src/main/java/components/shapes/Rectangle.java
@@ -1,9 +1,20 @@
 package components.shapes;
 
 
+import com.uqbar.vainilla.colissions.CollisionDetector;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class Rectangle implements Shape{
+public class Rectangle extends SimpleShape implements Shape {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
     public boolean isColliding(Shape shape) {
         return shape.collidedByRectangle(this);
@@ -11,7 +22,7 @@ public class Rectangle implements Shape{
 
     @Override
     public boolean collidedByCircle(Circle shape) {
-        throw new NotImplementedException();
+        return CollisionDetector.INSTANCE.collidesCircleAgainstRect(shape.getX(), shape.getY(), (int) shape.getRadio(), getX(), getY(), getWidth(), getHeight());
     }
 
     @Override
@@ -22,5 +33,21 @@ public class Rectangle implements Shape{
     @Override
     public boolean collidedByMultiShape(MultiShape multiShape) {
         throw new NotImplementedException();
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
     }
 }
--- asteroid/src/test/java/components/shapes/CircleTest.java
+++ asteroid/src/test/java/components/shapes/CircleTest.java
@@ -9,6 +9,10 @@ public class CircleTest extends TestCase {
     BasicAsteroidMovingGameComponent component2;
     Circle circle2;
 
+    BasicAsteroidMovingGameComponent component3;
+    Rectangle rectangle;
+
+
     public void setUp() {
         component = new BasicAsteroidMovingGameComponent(0, 0);
         circle = new Circle(20);
@@ -17,18 +21,32 @@ public class CircleTest extends TestCase {
         component2 = new BasicAsteroidMovingGameComponent(0, 0);
         circle2 = new Circle(20);
         circle2.setComponent(component2);
+
+        component3 = new BasicAsteroidMovingGameComponent(0, 0);
+        rectangle = new Rectangle(20, 10);
+        rectangle.setComponent(component3);
     }
 
     public void testCirclesCollided() {
         assertTrue(circle.isColliding(circle2));
     }
 
-    public void testCirclesCollidedWhenOneTouchsAnother(){
+    public void testCirclesCollidedWhenOneTouchsAnother() {
         component2.setX(10);
         assertTrue(circle.isColliding(circle2));
     }
-    public void testCirclesNotCollided(){
+
+    public void testCirclesNotCollided() {
         component2.setX(20);
         assertFalse(circle.isColliding(circle2));
+    }
+
+    public void testCircleCollidesRectangle() {
+        assertTrue(circle.isColliding(rectangle));
+    }
+
+    public void testCircleDoesntCollideRectangle() {
+        component3.setX(20);
+        assertFalse(circle.isColliding(rectangle));
     }
 }
