b88ffc6a1a7fb25f520740ccd60a53e72a123113
lalo <leandro.gz73@gmail.com>
2014-04-30 23:10:04 -0300

    Add relative X and Y for SimpleShapes. initialized on 0 for backward compatibility

--- asteroid/src/main/java/components/shapes/MultiShape.java
+++ asteroid/src/main/java/components/shapes/MultiShape.java
@@ -3,51 +3,49 @@ package components.shapes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MultiShape implements Shape{
-    private List<Shape> shapes;
+public class MultiShape implements Shape {
+	private List<Shape> shapes;
 
-    public MultiShape(){
-        setShapes(new ArrayList<Shape>());
-    }
+	public MultiShape() {
+		setShapes(new ArrayList<Shape>());
+	}
 
-    public void addShape(Shape shape){
-        getShapes().add(shape);
-    }
+	public void addShape(Shape shape) {
+		getShapes().add(shape);
+	}
 
-    @Override
-    public boolean isColliding(Shape shape) {
-        return shape.collidedByMultiShape(this);
-    }
+	@Override
+	public boolean isColliding(Shape shape) {
+		return shape.collidedByMultiShape(this);
+	}
 
-    @Override
-    public boolean collidedByCircle(Circle shape) {
-        boolean collided = false;
-        for(Shape s: getShapes())
-            collided |= shape.isColliding(s);
-        return collided;
-    }
+	@Override
+	public boolean collidedByCircle(Circle circle) {
+		return delagateOnChilden(circle);
+	}
 
-    @Override
-    public boolean collidedByRectangle(Rectangle rectangle) {
-        boolean collided = false;
-        for(Shape s: getShapes())
-            collided |= rectangle.isColliding(s);
-        return collided;
-    }
+	@Override
+	public boolean collidedByRectangle(Rectangle rectangle) {
+		return delagateOnChilden(rectangle);
+	}
 
-    @Override
-    public boolean collidedByMultiShape(MultiShape multiShape) {
-        boolean collided = false;
-        for(Shape s: getShapes())
-            collided |= multiShape.isColliding(s);
-        return collided;
-    }
+	@Override
+	public boolean collidedByMultiShape(MultiShape multiShape) {
+		return delagateOnChilden(multiShape);
+	}
 
-    public List<Shape> getShapes() {
-        return shapes;
-    }
+	public List<Shape> getShapes() {
+		return shapes;
+	}
 
-    public void setShapes(List<Shape> shapes) {
-        this.shapes = shapes;
-    }
+	public void setShapes(List<Shape> shapes) {
+		this.shapes = shapes;
+	}
+
+	private boolean delagateOnChilden(Shape shape) {
+		boolean collided = false;
+		for (Shape s : getShapes())
+			collided |= shape.isColliding(s);
+		return collided;
+	}
 }
--- asteroid/src/main/java/components/shapes/SimpleShape.java
+++ asteroid/src/main/java/components/shapes/SimpleShape.java
@@ -3,21 +3,50 @@ package components.shapes;
 
 import components.BasicAsteroidMovingGameComponent;
 
-public abstract class SimpleShape implements Shape{
-    BasicAsteroidMovingGameComponent component;
-    public BasicAsteroidMovingGameComponent getComponent() {
-        return component;
-    }
+public abstract class SimpleShape implements Shape {
+	BasicAsteroidMovingGameComponent component;
+	private double relativeX;
+	private double relativeY;
 
-    public void setComponent(BasicAsteroidMovingGameComponent component) {
-        this.component = component;
-    }
+	public SimpleShape() {
+		setRelativeX(0);
+		setRelativeY(0);
+	}
 
-    public double getX() {
-        return getComponent().getX();
-    }
+	public SimpleShape(double relativeX, double relativeY) {
+		setRelativeX(relativeX);
+		setRelativeY(relativeY);
+	}
 
-    public double getY() {
-        return getComponent().getY();
-    }
+	public BasicAsteroidMovingGameComponent getComponent() {
+		return component;
+	}
+
+	public void setComponent(BasicAsteroidMovingGameComponent component) {
+		this.component = component;
+	}
+
+	public double getX() {
+		return getComponent().getX() + getRelativeX();
+	}
+
+	public double getY() {
+		return getComponent().getY() + getRelativeY();
+	}
+
+	public double getRelativeY() {
+		return relativeY;
+	}
+
+	public void setRelativeY(double relativeY) {
+		this.relativeY = relativeY;
+	}
+
+	public double getRelativeX() {
+		return relativeX;
+	}
+
+	public void setRelativeX(double relativeX) {
+		this.relativeX = relativeX;
+	}
 }
