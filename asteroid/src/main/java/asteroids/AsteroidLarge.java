dbe0c57789dafa1696cc49d8526c361107e0780c
lalo <leandro.gz73@gmail.com>
2014-05-06 22:25:24 -0300

    Use Pool for getting AsteroidLarge

--- asteroid/src/main/java/asteroids/AsteroidLarge.java
+++ asteroid/src/main/java/asteroids/AsteroidLarge.java
@@ -1,40 +1,43 @@
 package asteroids;
 
-import java.util.Stack;
-
 import asteroid.AsteroidGame;
 
 import asteroids.factories.AsteroidLargeFactory;
+import asteroids.pools.AsteroidLargePool;
 
 public class AsteroidLarge extends Asteroid {
 
-	public static final Stack<AsteroidLarge> ASTEROIDS = new Stack<AsteroidLarge>();
+    private static AsteroidLargePool pool = new AsteroidLargePool();
 
 	public AsteroidLarge() {
 		super();
 	}
 
-	@Override
+    @Override
 	protected void explode() {
 		double radius = this.getGame().getSprite("asteroidM").getWidth() / 2;
 		double x = this.getX() + this.getWidth() / 2 - radius / 2;
 		double y = this.getY() + this.getHeight() / 2 - radius / 2;
 		this.getScene().addAsteroid(
-				AsteroidMedium.getPool().get(this.getGame(), x, y, this.getPi()));
+                AsteroidMedium.getPool().get(this.getGame(), x, y, this.getPi()));
 		this.getScene().addAsteroid(
 				AsteroidMedium.getPool().get(this.getGame(), x, y, this.getPi()));
 	}
 
 	@Override
 	public void store() {
-		ASTEROIDS.push(this);
+        getPool().push(this);
 	}
 
 	public static AsteroidLarge get(AsteroidGame game) {
-		if (ASTEROIDS.empty()) {
+		if (getPool().empty()) {
 			return AsteroidLargeFactory.newAsteroid(game);
 		}
-		return AsteroidLargeFactory.clean(ASTEROIDS.pop(), game);
+		return AsteroidLargeFactory.clean(getPool().pop(), game);
 	}
+
+    public static AsteroidLargePool getPool() {
+        return pool;
+    }
 
 }
---
+++ asteroid/src/main/java/asteroids/pools/AsteroidLargePool.java
@@ -0,0 +1,8 @@
+package asteroids.pools;
+
+import asteroids.AsteroidLarge;
+
+public class AsteroidLargePool extends AsteroidPool<AsteroidLarge>{
+
+
+}
