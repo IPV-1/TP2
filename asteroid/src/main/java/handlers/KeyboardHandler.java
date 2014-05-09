522abdf482ed85131f0bf1d4088d756971ae6e07
lalo <leandro.gz73@gmail.com>
2014-05-08 23:52:09 -0300

    smarter keyboard

--- asteroid/src/main/java/handlers/KeyboardHandler.java
+++ asteroid/src/main/java/handlers/KeyboardHandler.java
@@ -6,26 +6,26 @@ import com.uqbar.vainilla.DeltaState;
 import com.uqbar.vainilla.events.constants.Key;
 
 public class KeyboardHandler {
-	
-	public static final KeyboardHandler INSTANCE = new KeyboardHandler();
 
-	private KeyboardHandler() {		
-	}
+    public static final KeyboardHandler INSTANCE = new KeyboardHandler();
 
-	public void updateShip(Ship ship, DeltaState deltaState) {
-		if(deltaState.isKeyBeingHold(Key.LEFT)) {
-        	ship.rotate(-1);
-        } else if(deltaState.isKeyBeingHold(Key.RIGHT)) {
-        	ship.rotate(1);
-        } else if(deltaState.isKeyBeingHold(Key.UP)) {
-        	ship.setMaxSpeed();
-        } else if(deltaState.isKeyReleased(Key.SPACE)) {
-        	ship.shot();
+    private KeyboardHandler() {
+    }
+
+    public void updateShip(Ship ship, DeltaState deltaState) {
+        if (deltaState.isKeyBeingHold(Key.LEFT)) {
+            ship.rotate(-1);
+        } else if (deltaState.isKeyBeingHold(Key.RIGHT)) {
+            ship.rotate(1);
         }
-		
-		if(! deltaState.isKeyBeingHold(Key.UP) && ship.getSpeed() > 0) {
-        	ship.breakingSpeed(deltaState);
-		}
-	}
+        if (deltaState.isKeyBeingHold(Key.UP)) {
+            ship.setMaxSpeed();
+        } else if (ship.getSpeed() > 0) {
+            ship.breakingSpeed(deltaState);
+        }
+        if (deltaState.isKeyReleased(Key.SPACE)) {
+            ship.shot();
+        }
+    }
 
 }
