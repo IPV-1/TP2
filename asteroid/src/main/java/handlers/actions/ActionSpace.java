57d794dabee97c70f1b373e153d147c30d7c522a
Mariano <marianolinares92@gmail.com>
2014-05-18 18:56:33 -0300

    Se dispara en el release del space

--- asteroid/src/main/java/handlers/actions/ActionSpace.java
+++ asteroid/src/main/java/handlers/actions/ActionSpace.java
@@ -3,8 +3,12 @@ package handlers.actions;
 import ship.Ship;
 
 public class ActionSpace extends Action {
+
 	@Override
-	public void execute(Ship ship, double delta) {
-		ship.shot();
+	public void setSelected(Ship ship, boolean selected) {
+		if(!selected) {
+			ship.shot();
+		}
 	}
+	
 }
